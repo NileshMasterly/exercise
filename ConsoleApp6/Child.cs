2b7efa21fec96060a1c427fb5e833ded34e74625
Nilesh <Nilesh.n@masterlysolutions.com>
2023-01-18 13:42:39 +0530

    program 6

---
+++ ConsoleApp6/Child.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+     class Child : Parent
+    {
+        public void PrintChild()
+        {
+            Console.WriteLine("This is Child class");
+        }
+    }
+}
--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp6;
+
 Console.WriteLine("Exersice 6");
+
+Parent parent = new Parent();
+Child child = new Child();
+
+parent.PrintParent();
+child.PrintChild();
+child.PrintParent();
