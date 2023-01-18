a8f750b1bbe1d6f6c6a985999af841c6dd6694c1
Nilesh <Nilesh.n@masterlysolutions.com>
2023-01-18 14:07:55 +0530

    execrise 3

---
+++ ConsoleApp7/Member.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Member
+    {
+        string Name;
+        int Age;
+        string PhoneNumber;
+        string Address;
+        long Salary;
+
+
+        public void PrintSalary()
+        {
+            Console.WriteLine(Salary.ToString());
+        }
+
+    }
+}
